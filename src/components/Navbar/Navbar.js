9ee719aab610bbd3f395b90b65e403f03e90f18d
MoSuperCode <mo.supercode@gmail.com>
2022-12-12 15:03:25 +0100

    GameCard

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.js';
+
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img></img>
+            <h1></h1>
+        </nav>
+    );
+}
