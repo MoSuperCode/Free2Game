46da96e4490f4e638ad5450e00f6680b479e6de5
MoSuperCode <mo.supercode@gmail.com>
2022-12-12 13:23:05 +0100

    GameCard

--- src/components/GameCard/GameCard.js
+++ src/components/GameCard/GameCard.js
@@ -1,14 +1,26 @@
 import "./GameCard.css";
 
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function GameCard(props) {
+    const [platformSrc, setPlatform] = useState("");
+    if (props.platform === "Web Broser") {
+        setPlatform("../../assets/img/Web.svg");
+    } else if (props.platform === "PC (Windows)") {
+        setPlatform("../../assets/img/Windows.svg");
+    }
     return (
-        <div>
-            <img src={props.src}></img>
-            <h2>{props.title}</h2>
-            <p>{ }</p>
-
+        <div className="gameCards">
+            <img src={props.thumbnail}></img>
+            <div>
+                <h2>{props.title}</h2>
+                <p>{props.short_description}</p>
+                <button>Read More</button>
+            </div>
+            <div className="gameCardFlex">
+                <img src={platformSrc}></img>
+                <h3>{props.genre}</h3>
+            </div>
         </div>
     );
 }
