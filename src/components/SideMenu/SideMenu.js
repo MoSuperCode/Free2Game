105ad67f39eddb159e33c832d5472a5242bdf8a6
MoSuperCode <mo.supercode@gmail.com>
2022-12-12 17:25:08 +0100

    .

---
+++ src/components/SideMenu/SideMenu.js
@@ -0,0 +1,12 @@
+import './SideMenu.css';
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function SideMenu() {
+    return (
+        <side className="sideMenu">
+            <Link to="{}" ><img></img></Link>
+        </side>
+    );
+}
