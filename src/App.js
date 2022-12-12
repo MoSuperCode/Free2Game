6dd5bcf4fcf71cc0c45c7b561a8c951a3335db88
namlidN <dilman.n999@gmail.com>
2022-12-12 15:23:47 +0100

    sdfg

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+
+      
       <Router>
         <Routes>
      
 
         </Routes>
-      </Router> */}
+      </Router> 
     </div>
   );
 };
