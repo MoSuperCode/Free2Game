c3ceb7f9a99c85c460ab8d9dc36cfc2fa3f59587
MoSuperCode <mo.supercode@gmail.com>
2022-12-13 18:53:20 +0100

    test deplay

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -2,7 +2,7 @@ import './SideMenu.css';
 import Homepic from '../../assets/img/Home.png';
 import Controller from '../../assets/img/Controller.png';
 import controls from '../../assets/img/controls.png';
-import hamburger from '../../assets/img/burger.png';
+
 
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -10,7 +10,7 @@ import { Link } from 'react-router-dom';
 export default function SideMenu() {
     return (
         <div className='border'>
-            <img alt='.' className='Hamburger' src={hamburger}></img>
+
             <div className="sideMenu">
                 <Link to="/" ><img alt='.' src={Homepic}></img></Link>
                 <Link to="/allgames" ><img alt='.' src={Controller}></img></Link>
