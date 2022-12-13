1b8874b233febdaf44dec4be7b29a24ee9535532
MoSuperCode <mo.supercode@gmail.com>
2022-12-13 11:25:12 +0100

    sidebar

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import Home from './pages/Home';
 import AllGames from './pages/AllGames';
 import DetailView from './pages/DetailView';
 import RecentlyAdded from './pages/RecentlyAdded';
+import SideMenu from './components/SideMenu/SideMenu';
 
 function App() {
   return (
     <div className="App">
-  
+
       <Router>
         <Routes>
-          <Route path="/" element={<> <Navbar /> <Home /> </>} />
-          <Route path="/allgames" element={<> <Navbar /> <AllGames /> </>} />
-          <Route path="recentlyadded/detailview/:id" element={<> <Navbar /> <DetailView /> </>} />
-          <Route path="/recentlyadded" element={<> <Navbar /> <RecentlyAdded /> </>} />
+          <Route path="/" element={<> <Navbar /> <SideMenu /> <Home /> </>} />
+          <Route path="/allgames" element={<> <Navbar /> <SideMenu /> <AllGames /> </>} />
+          <Route path="recentlyadded/detailview/:id" element={<> <Navbar /> <SideMenu /> <DetailView /> </>} />
+          <Route path="/recentlyadded" element={<> <Navbar /> <SideMenu /> <RecentlyAdded /> </>} />
         </Routes>
       </Router>
     </div>
