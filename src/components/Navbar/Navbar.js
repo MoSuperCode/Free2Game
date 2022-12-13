c6a705ad18e23dcab85462b47e766759650718f0
MoSuperCode <mo.supercode@gmail.com>
2022-12-13 12:32:59 +0100

    SideMenu and Navbar Changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,13 +3,14 @@ import React from 'react';
 import Logo from '../../assets/img/Logo.svg';
 
 export default function Navbar() {
+
     return (
         <nav className="navbarFlex">
             <div className="flex">
                 <img src={Logo} alt="s"></img>
                 <h1>FREE2GAME</h1>
             </div>
-            <input placeholder='🔎' style={{ textAlign: 'right' }} ></input>
+            <input className='input' placeholder='🔎' style={{ textAlign: 'right' }} ></input>
         </nav >
     );
 }
--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -2,16 +2,20 @@ import './SideMenu.css';
 import Homepic from '../../assets/img/Home.png';
 import Controller from '../../assets/img/Controller.png';
 import controls from '../../assets/img/controls.png';
+import Hamburger from '../../assets/img/Burger.png';
 
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function SideMenu() {
     return (
-        <side className="sideMenu">
-            <Link to="/" ><img src={Homepic}></img></Link>
-            <Link to="/allgames" ><img src={Controller}></img></Link>
-            <Link to="/recentylyadded" ><img src={controls}></img></Link>
-        </side>
+        <div className='border'>
+            <img className='Hamburger' src={Hamburger}></img>
+            <div className="sideMenu">
+                <Link to="/" ><img src={Homepic}></img></Link>
+                <Link to="/allgames" ><img src={Controller}></img></Link>
+                <Link to="/recentlyadded" ><img src={controls}></img></Link>
+            </div>
+        </div>
     );
 }
